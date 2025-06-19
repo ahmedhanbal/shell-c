e178330193bdf4403c95c7bba1022bc6a7bea382
ahmedhanbal <ahmed.alizahid14@gmail.com>
2025-06-19 12:53:06 +0500

    test3

--- src/main.c
+++ src/main.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-
-  // Wait for user input
+  // input buffer
   char input[100];
-  if (fgets(input, sizeof(input), stdin)) {
-    // this handles the case if input entered is larger than buffer size and there is leftover in stdin
-    // Check if newline is present
-    if (!strchr(input, '\n')) {
+  while (1)
+  {
+    printf("$ ");
+    if (fgets(input, sizeof(input), stdin))
+    {
+      // this handles the case if input entered is larger than buffer size and there is leftover in stdin
+      // Check if newline is present
+      if (!strchr(input, '\n'))
+      {
         // Newline not found → line was too long, flush remainder
         int ch;
-        while ((ch = getchar()) != '\n' && ch != EOF);
+        while ((ch = getchar()) != '\n' && ch != EOF)
+          ;
+      }
+      // remove newline from input
+      input[strcspn(input, "\n")] = '\0';
+      printf("%s: command not found\n", input);
     }
-    // remove newline from input
-    input[strcspn(input, "\n")] = '\0';
-}
+    else {
+      printf("Error or EOF occurred\n");
+    }
+  }
 
-  printf("%s: command not found\n",input);
+  
   return 0;
 }

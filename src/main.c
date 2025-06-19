5abd6c3574dcc8ef3693fb2e9ce2a9ebab3c39d1
ahmedhanbal <ahmed.alizahid14@gmail.com>
2025-06-19 12:42:27 +0500

    test2-pass

--- src/main.c
+++ src/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
   printf("$ ");
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
+  if (fgets(input, sizeof(input), stdin)) {
+    // this handles the case if input entered is larger than buffer size and there is leftover in stdin
+    // Check if newline is present
+    if (!strchr(input, '\n')) {
+        // Newline not found → line was too long, flush remainder
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF);
+    }
+    // remove newline from input
+    input[strcspn(input, "\n")] = '\0';
+}
 
   printf("%s: command not found\n",input);
   return 0;
